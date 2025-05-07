ed98e871d51151f6d40fee5b92be1f86df8232a7
Ulszka <ulszkahand@gmail.com>
2025-05-07 21:58:19 +0200

    updating photos

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,18 +3,19 @@ import happy from "../../assets/my_pictures/chosen/happy.jpg";
 import mountain from "../../assets/my_pictures/chosen/mountain.jpg";
 import angry from "../../assets/my_pictures/chosen/angry.jpg";
 import lighthouse from "../../assets/my_pictures/chosen/lighthouse_edit.jpg";
-// import rocks from "../../assets/my_pictures/chosen/question.jpg";
+import motorcycle from "../../assets/my_pictures/chosen/motorcycle.jpg";
+import ukulele from "../../assets/my_pictures/chosen/ukulele.jpg";
 import "./Banner.scss";
 
 const Banner = () => {
-  const pictures: string[] = [happy, mountain, angry, lighthouse];
+  const pictures: string[] = [ukulele, happy, mountain, angry, lighthouse, motorcycle];
   const traits: string[] = [
+    "it's time off",
     "new project starts",
     "difficulties show up",
     "work doesn't get done",
     "project turns out to be bigger than expected",
-    // "it's time to recharge"
-    // "it's time for adventure"
+    "it's time for adventure",
   ];
 
   const [index, setIndex] = useState<number>(0);
